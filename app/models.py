bfc92ac64711820652f58148baacfa04801926d2
Logan Gorence <logan@gorence.io>
2020-10-19 09:37:23 -0700

    Add basic user model.

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db, login
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+    is_professor = db.Column(db.Boolean())
+
+    def __repr__(self):
+        return '<{} {}-{}>'.format("Professor" if self.is_professor else "Student", self.id, self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
