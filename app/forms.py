41307a9fedb0db476c25625b2e3a69e847790d54
Logan Gorence <logan@gorence.io>
2020-11-10 17:03:25 -0800

    Basic form for create course and model.

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import  ValidationError, DataRequired, EqualTo, Length, 
 from wtforms.widgets import ListWidget, CheckboxInput
 from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
 
-from app import db
 from app.models import User
+
 
 class RegisterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -19,19 +19,44 @@ class RegisterForm(FlaskForm):
         if user is not None:
             raise ValidationError('Email is already in use. Forgot password?')
 
+
 class LoginForm(FlaskForm):
     email = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Login')
 
+
 class InfoForm(FlaskForm):
     name = StringField('Name', validators=[Length(max=70)])
     phone = IntegerField('Phone', validators=[Optional()])
-    wsuID = StringField('WSU ID',validators=[Length(max =8)])
+    wsuID = StringField('WSU ID',validators=[Length(max=8)])
 
-    #additional information (students only)
+    # additional information (students only)
     gpa = DecimalField('GPA',validators=[Optional()])
     major = StringField('Major', validators=[Optional()])
     graduation = DateField('Expected Graduation', format ='%m/%Y', validators=[Optional()])
     submit = SubmitField('Save')
+
+
+class CreateCourseForm(FlaskForm):
+    name = StringField('Name', validators=[Length(max=64), DataRequired()])
+    description = StringField('Description', validators=[Length(max=1024), DataRequired()])
+    semester = SelectField('Semester', choices=[
+        'Spring',
+        'Summer',
+        'Fall'
+    ], validators=[DataRequired()])
+    year = SelectField('Year', choices=[
+        '2020',
+        '2021',
+        '2022',
+        '2023',
+        '2024',
+        '2025',
+        '2026',
+        '2027',
+        '2028',
+        '2029'
+    ])
+    submit = SubmitField('Create')
--- app/routes.py
+++ app/routes.py
@@ -3,17 +3,19 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 from app import app, db
 from app.models import User
-from app.forms import LoginForm, RegisterForm, InfoForm
+from app.forms import LoginForm, RegisterForm, InfoForm, CreateCourseForm
 
 @app.before_first_request
 def initDB(*args, **kwargs):
     db.create_all()
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     return render_template('index.html', user=current_user)
+
 
 @app.route('/student/register', methods=['GET', 'POST'])
 def student_register():
@@ -32,6 +34,7 @@ def student_register():
 
     return render_template('register.html', form=form)
 
+
 @app.route('/professor/register', methods=['GET', 'POST'])
 def professor_register():
     if current_user.is_authenticated:
@@ -48,6 +51,18 @@ def professor_register():
         return redirect(url_for('index'))
 
     return render_template('register.html', form=form)
+
+
+@app.route('/professor/courses/create')
+@login_required
+def professor_create_course():
+    form = CreateCourseForm()
+
+    if form.validate_on_submit():
+        pass
+
+    return render_template('professor/create_course.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -66,6 +81,7 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', form=form)
+
 
 @app.route('/info/', methods=['GET', 'POST'])
 @login_required
@@ -89,6 +105,7 @@ def info():
         db.session.commit()
         return redirect(url_for('info'))
     return render_template('info.html', form = form)
+
 
 @app.route('/logout', methods=['GET'])
 def logout():
